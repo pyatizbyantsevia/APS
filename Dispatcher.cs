b274478349db03552b513f83106f32965aa5df1b
Илья <ilya_2.0@mail.ru>
2023-05-12 11:04:54 +0300

    Add classes

---
+++ Dispatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+namespace APS
+{
+    class Dispatcher
+    {
+        public void Dispatch(Task callerTask)
+        {
+            new Thread(delegate ()
+            {
+                Task taskToStartOrResume = OS.Scheduler.PopHighestPriorityTask();
+                if (taskToStartOrResume != null)
+                    taskToStartOrResume.StartOrResume();
+            }).Start();
+            if (callerTask != null) 
+                callerTask.Yield();
+        }
+    }
+}
+
---
+++ OS.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APS
+{
+    class OS
+    {
+        private static Scheduler _scheduler = new Scheduler();
+        public static Scheduler Scheduler
+        {
+            get
+            {
+                return _scheduler;
+            }
+        }
+        private static Dispatcher _dispatcher = new Dispatcher();
+
+        public static Dispatcher Dispatcher
+        {
+            get
+            {
+                return _dispatcher;
+            }
+        }
+        public static void Print(string text)
+        {
+            _osForm.AppendToOutput(text);
+        }
+        private static RtosForm _osForm = null;
+        public OS(RtosForm osForm)
+        {
+            _osForm = osForm;
+        }
+
+        private List<Task> _taskPool = new List<Task>();
+
+        public List<Task> taskPool
+        {
+            get
+            {
+                return _taskPool;
+            }
+        }
+
+        public List<Semaphore> _semaphorePool = new List<Semaphore>();
+        public List<Semaphore> SemaphorePool
+        {
+            get
+            {
+                return _semaphorePool;
+            }
+        }
+
+        public void Start()
+        {
+            if (_taskPool.count > 0)
+            {
+                _taskPool[0].Activate(null);
+            }
+        }
+
+        public void Shutdown()
+        {
+            for (int i = 0; i < _taskPool.Count; i++)
+            {
+                try
+                {
+                    taskPool[1].ThreadToRun.Abort();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                taskPool.Clear();
+                _semaphorePool.Clear();
+
+            }
+        }
+    }
+}
